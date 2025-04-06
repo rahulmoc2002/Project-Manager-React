cf6da21ba70642318ffc53514fe745b2a47803b7
rahul purigilla <76097290+rahulmoc2002@users.noreply.github.com>
2025-04-06 06:29:05 +0000

    Changes2

--- src/Components/NewProject.jsx
+++ src/Components/NewProject.jsx
@@ -1,5 +1,19 @@
+import { useRef } from "react";
 import Input from "./Input";
-export default function NewProject() {
+export default function NewProject({ onAdd }) {
+  const title = useRef();
+  const description = useRef();
+  const duedate = useRef();
+  function handleSave() {
+    const enteredTitle = title.current.value;
+    const enteredDescription = title.current.value;
+    const enteredDuedate = title.current.value;
+    onAdd({
+      title: enteredTitle,
+      desc: enteredDescription,
+      due: enteredDuedate,
+    });
+  }
   return (
     <div className="w-[35rem] mt-16">
       <menu className="flex itemsn-center justify-end gap-4 my-4">
@@ -9,15 +23,18 @@ export default function NewProject() {
           </button>
         </li>
         <li>
-          <button className="px-6 py-2 rounded-md bg-stone-800 text-stone-50 hover:bg-stone-950">
+          <button
+            onClick={handleSave}
+            className="px-6 py-2 rounded-md bg-stone-800 text-stone-50 hover:bg-stone-950"
+          >
             Save
           </button>
         </li>
       </menu>
       <div>
-        <Input label="Title" />
-        <Input textarea label="Desc" />
-        <Input label="Due date" />
+        <Input ref={title} label="Title" />
+        <Input ref={description} textarea label="Desc" />
+        <Input ref={duedate} label="Due date" />
       </div>
     </div>
   );
